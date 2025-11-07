d6e5257610422e6fcbd44bae84b49865c38e31bb
Snyzell <121956299+Snyzell@users.noreply.github.com>
2025-11-07 21:45:39 +0100

    1ère gen (Sans type)

--- Assets/prefab/PokeUnit.cs
+++ Assets/prefab/PokeUnit.cs
@@ -15,27 +15,50 @@ public class PokeUnit : ScriptableObject
 
     [Header("Statistiques de base")]
     [Tooltip("Points de vie")]
-    public int pv = 100;
+    public int pv = 0;
+
+    [Tooltip("Puissance des attaques physiques")]
+    public int attaque = 0;
+
+    [Tooltip("Défense contre les attaques ennemies")]
+    public int defense = 0;
 
     [Tooltip("Puissance des attaques spéciales")]
-    public int attaqueSpeciale = 50;
-
-    [Tooltip("Défense contre les attaques ennemies")]
-    public int defense = 40;
+    public int attaqueSpeciale = 0;
     
     [Tooltip("Défense contre les attaques ennemies")]
-    public int defenseSpeciale = 40;
+    public int defenseSpeciale = 0;
 
     [Tooltip("Vitesse d'exécution ou d'esquive")]
-    public int vitesse = 60;
+    public int vitesse = 0;
 
     [Header("Type élémentaire")]
     [Tooltip("Type principal de la créature (ex: Feu, Eau, Plante, etc.)")]
     public PokeType type = PokeType.Normal;
+    public PokeType2 PokeType2 = PokeType2.Normal;
 }
 
 public enum PokeType
 {
+    Normal,
+    Feu,
+    Eau,
+    Plante,
+    Électrik,
+    Roche,
+    Sol,
+    Glace,
+    Combat,
+    Psy,
+    Spectre,
+    Dragon,
+    Ténèbres,
+    Fée
+}
+
+public enum PokeType2
+{
+    None,
     Normal,
     Feu,
     Eau,
